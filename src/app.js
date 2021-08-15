6c4a7593880d3dab2ae0807f5ed70bc014a2e33e
Amirmardan-prog <88323126+Amirmardan-prog@users.noreply.github.com>
2021-08-15 19:47:52 -0400

    FIX THE STYLE OF INPUT RANGE

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 
 document.getElementById("myInput").oninput = function() {
     var value = (this.value-this.min)/(this.max-this.min)*100
-    console.log(this)
     this.style.background = 'linear-gradient(to right, #82CFD0 0%, #82CFD0 ' + value + '%, hsl(224, 65%, 95%) ' + value + '%, hsl(224, 65%, 95%) 100%)'
 };
